e7c51781b760ae6d58e3875f210e3b8464afd9fb
Timofey <timofeykrutov@gmail.com>
2018-07-17 21:26:13 +0300

    add hi github!

--- src/sfg.java
+++ src/sfg.java
@@ -1,6 +1,7 @@
 public class sfg {
     public void print(){
-        System.out.print("HELLO WORLD");
+        System.out.print("HELLO WORLD ");
+        System.out.print("HI GitHub! ");
     }
 
 }
